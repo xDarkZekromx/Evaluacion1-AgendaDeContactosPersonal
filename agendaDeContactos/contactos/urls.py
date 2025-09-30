7853a4c50f9105b2789a2b6d9f574b53c216135b
xDarkZekromx <marcelohermosillamolina@hotmail.com>
2025-09-30 02:05:04 -0300

    2
    2

--- agendaDeContactos/contactos/urls.py
+++ agendaDeContactos/contactos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contactos/', views.lista_contactos, name='lista_contactos'),
-    path('contactos/nuevo/', views.nuevo_contacto, name='nuevo_contacto'),
-    path('contactos/<int:pk>/', views.detalle_contacto, name='detalle_contacto'),
-    path('contactos/<int:pk>/editar/', views.editar_contacto, name='editar_contacto'),
-    path('contactos/<int:pk>/eliminar/', views.eliminar_contacto, name='eliminar_contacto'),
+    path('personas/', views.lista_contactos, name='lista_contactos'),
+    path('personas/nuevo/', views.nuevo_contacto, name='nuevo_contacto'),
+    path('personas/<int:pk>/', views.detalle_contacto, name='detalle_contacto'),
+    path('personas/<int:pk>/editar/', views.editar_contacto, name='editar_contacto'),
+    path('personas/<int:pk>/eliminar/', views.eliminar_contacto, name='eliminar_contacto'),
 ]
--- agendaDeContactos/contactos/views.py
+++ agendaDeContactos/contactos/views.py
@@ -6,37 +6,37 @@ from .form import ContactoForm
 
 def lista_contactos(request):
     contactos = Contacto.objects.all()
-    return render(request, 'contactos/lista_contactos.html', {'contactos': contactos})
+    return render(request, 'personas/lista_contactos.html', {'contactos': contactos})
 
 def detalle_contacto(request, pk):
     contacto = get_list_or_404(Contacto, pk=pk)
-    return render(request, 'contactos/detalle_contacto.html', {'contacto': contacto})
+    return render(request, 'personas/detalle_contacto.html', {'contacto': contacto})
 
 def nuevo_contacto(request):
     if request.method == "POST":
         form = ContactoForm(request.POST)
         if form.is_valid():
-            contacto = form.save()
-            return redirect('detalle_contacto', pk=contacto.pk)
+            form.save()
+            return redirect('detalle_contacto')
     else:
         form = ContactoForm()
-    return render(request, 'contactos/nuevo_contacto.html', {'form': form})
+    return render(request, 'personas/form_contactos.html', {'form': form})
 
 def editar_contacto(request, pk):
     contacto = get_list_or_404(Contacto, pk=pk)
     if request.method == "POST":
         form = ContactoForm(request.POST, instance=contacto)
         if form.is_valid():
-            contacto = form.save()
-            return redirect('detalle_contacto', pk=contacto.pk)
+            form.save()
+            return redirect('detalle_contacto')
     else:
         form = ContactoForm(instance=contacto)
-    return render(request, 'contactos/editar_contacto.html', {'form': form})
+    return render(request, 'personas/form_contactos.html', {'form': form})
 
 def eliminar_contacto(request, pk):
     contacto = get_list_or_404(Contacto, pk=pk)
     if request.method == "POST":
         contacto.delete()
         return redirect('lista_contactos')
-    return render(request, 'contactos/eliminar_contacto.html', {'contacto': contacto})
+    return render(request, 'personas/contacto_confirm_delete.html', {'contacto': contacto})
 
